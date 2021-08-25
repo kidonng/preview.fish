98e83f9ea704899d3d29c68973d20815b32e7f2c
Kid <kidonng@gmail.com>
2021-08-25 22:51:24 +0800

    Support type matching

--- functions/preview.fish
+++ functions/preview.fish
@@ -18,15 +18,19 @@ function preview -d "Preview file content"
             continue
         end
 
+        set -l type (file -b $arg | string replace -a " " _)
         set -l mime (file -b --mime-type $arg)
         set -l mime_base (string split -f 1 / $mime[1])
         set -l mime_name (string replace / _ $mime[1])
 
-        if functions -q _preview_mime_$mime_base
-            _preview_mime_$mime_base $arg
+        if functions -q _preview_type_$type
+            _preview_type_$type $arg
             continue
         else if functions -q _preview_mime_$mime_name
             _preview_mime_$mime_name $arg
+            continue
+        else if functions -q _preview_mime_$mime_base
+            _preview_mime_$mime_base $arg
             continue
         end
 
