3e1173b87bf7ed367d61b782a454982a8f80aaf8
Kid <kidonng@gmail.com>
2021-08-25 23:00:04 +0800

    Improve JSON and JS matching

--- functions/_preview_mime_application_javascript.fish
+++ functions/_preview_mime_application_javascript.fish
@@ -1,1 +1,5 @@
-_preview_viewer_bat.fish
+functions -q _preview_viewer_bat || exit
+
+function _preview_mime_application_json
+    _preview_viewer_bat -l js $argv
+end
--- functions/_preview_mime_application_json.fish
+++ functions/_preview_mime_application_json.fish
@@ -1,4 +1,4 @@
-command -sq bat || exit
+functions -q _preview_viewer_bat || exit
 
 function _preview_mime_application_json
     _preview_viewer_bat -l json $argv
