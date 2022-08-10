a5983552cd1b19a27e983748954367b33065b47c
Kid <hi@xuann.wang>
2022-08-10 13:43:02 +0800

    Format

--- functions/_preview_ext_md.fish
+++ functions/_preview_ext_md.fish
@@ -1,11 +1,11 @@
 if ! functions -q _preview_viewer_glow && ! functions -q _preview_viewer_bat
-	exit
+    exit
 end
 
 function _preview_ext_md
-	if functions -q _preview_viewer_glow
-		_preview_viewer_glow $argv
-	else 
-		_preview_viewer_bat $argv
-	end
+    if functions -q _preview_viewer_glow
+        _preview_viewer_glow $argv
+    else
+        _preview_viewer_bat $argv
+    end
 end
