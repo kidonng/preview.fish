995e741476760652f5f8d6f28438cf486caab50c
Kid <hi@xuann.wang>
2022-08-10 12:16:47 +0800

    Format

--- functions/_preview_mime_application_gzip.fish
+++ functions/_preview_mime_application_gzip.fish
@@ -1,7 +1,7 @@
 function _preview_mime_application_gzip
-	if string match -q "*.tar.gz" $argv || test (path extension $argv) = .tgz
-		_preview_viewer_tar $argv
-	else
-		_preview_viewer_gzip $argv
-	end
+    if string match -q "*.tar.gz" $argv || test (path extension $argv) = .tgz
+        _preview_viewer_tar $argv
+    else
+        _preview_viewer_gzip $argv
+    end
 end
--- functions/_preview_mime_application_zstd.fish
+++ functions/_preview_mime_application_zstd.fish
@@ -1,7 +1,7 @@
 function _preview_mime_application_zstd
-	if string match -q "*.tar.zst" $argv
-		_preview_viewer_tar $argv
-	else
-		_preview_viewer_zstd $argv
-	end
+    if string match -q "*.tar.zst" $argv
+        _preview_viewer_tar $argv
+    else
+        _preview_viewer_zstd $argv
+    end
 end
