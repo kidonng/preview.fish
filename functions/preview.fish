96de360f0ff4a150713cd4203682b12fe99063f0
Kid <hi@xuann.wang>
2022-08-10 12:43:04 +0800

    Improve filename in preview header

--- functions/preview.fish
+++ functions/preview.fish
@@ -1,17 +1,15 @@
 function preview -d "Preview file content"
-    test (count $argv) != 1 && set -l multi
-    set -l newline
-
     for arg in $argv
-        if test "$newline" = 1
-            echo
+        set_color -o
+        # Show filename with LS_COLORS
+        if command -sq exa
+            exa --color always $arg
         else
-            set newline 1
-        end
+            ls $arg
+        end | string replace $HOME "~"
+        set_color normal
 
-        set -q multi && echo (set_color -o)$arg(set_color normal)
-
-        set -l extname (string match -r -- '[^.]+$' $arg)
+        set -l extname (string replace . "" (path extension $arg))
 
         if functions -q _preview_ext_$extname
             _preview_ext_$extname $arg
@@ -19,23 +17,24 @@ function preview -d "Preview file content"
         end
 
         set -l type (file -b $arg)
-        set -l type_name (string replace -a " " _ $type)
+        set -l type_name (string replace -a -- " " _ $type)
         set -l mime (file -b --mime-type $arg)
         set -l mime_base (string split -f 1 / $mime[1])
         set -l mime_name (string replace / _ $mime[1])
 
         if functions -q _preview_type_$type_name
             _preview_type_$type_name $arg
-            continue
         else if functions -q _preview_mime_$mime_name
             _preview_mime_$mime_name $arg
-            continue
         else if functions -q _preview_mime_$mime_base
             _preview_mime_$mime_base $arg
-            continue
+        else
+            echo $type
+            echo "($mime[1])"
         end
 
-        echo $type
-        echo "($mime[1])"
+        if test "$arg" != "$argv[-1]"
+            printf \n
+        end
     end
 end
